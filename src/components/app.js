a9b30f43bb3df4c1ed815cabd5fce68f9d16064a
anhhtle <anh.ht.le@gmail.com>
2017-06-28 21:21:13 -0700

    new

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 
-import Header from './header';
-import Navigation from './navigation';
-import MainContainer from './main-container';
-import DetailContainer from './detail-container';
-import SpeechTranscript from './speech-transcript';
+// import Header from './header';
+// import Navigation from './navigation';
+// import MainContainer from './main-container';
+// import DetailContainer from './detail-container';
+// import SpeechTranscript from './speech-transcript';
 //import WatsonDetailContainer from './watson-detail-container';
-import UserTextContainer from  './user-text-container';
+// import UserTextContainer from  './user-text-container';
 import LandingPage from './landing-page';
 
 
@@ -15,14 +15,14 @@ export default function App() {
 
     return (
         <div className="App">
-            <Header />
+            {/*<Header />*/}
             <LandingPage />
-            <Route path="/" component={Navigation}/>
+            {/*<Route path="/" component={Navigation}/>
             <Route exact path="/" component={MainContainer}/>
             <Route path="/:userText" component={Navigation}/>
             <Route exact path="/:userText" component={UserTextContainer} />
             <Route exact path="/detail/:presid" component={DetailContainer} />
-            <Route exact path="/transcript/:presid/:speechid" component={SpeechTranscript} />
+            <Route exact path="/transcript/:presid/:speechid" component={SpeechTranscript} />*/}
         </div>
     );
 };
--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './styles/landing-page.css';
 
@@ -6,7 +7,27 @@ export default function LandingPage(){
 
     return(
         <div className="landing-page-container">
-            <img src="../public/img/landing_image.png" alt="landing_image"/>
+            <section className="section-one">
+                <h3>In-depth Analysis of Presidential Speeches</h3>
+            </section>
+            <div className="ibm-div">
+                <p>Powered by <span className="ibm">IBM Watson</span></p>
+            </div>
+            <section className="section-two">
+                <div>
+                    <h3>Comprehensive database</h3>
+                    <p>Easily browse and view transcript of famous speeches. Use <span className="watson">Watson</span> 
+                        <span className="personality-insights">Personality Insight</span> to analyze the transcript based on: Big Five personality, Needs, and Values.
+                    </p>
+                    <Link to="">Get Started</Link>
+                </div>
+            </section>
+            <section className="section-three">
+                <div className="left"/>
+                <div className="center">
+                    <p>Use Watson Personality Insight on your own text of interest</p>
+                </div>
+            </section>
         </div>
     )
 }
