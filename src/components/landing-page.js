5b45614cb1875c161273f792724bb2766e3aab0a
anhhtle <anh.ht.le@gmail.com>
2017-06-29 01:58:08 -0700

    new

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -23,11 +23,27 @@ export default function LandingPage(){
                 </div>
             </section>
             <section className="section-three">
-                <div className="left"/>
+                <div className="left" />
                 <div className="center">
-                    <p>Use Watson Personality Insight on your own text of interest</p>
+                    <p>Use Watson Personality Insight on your own text of interest.</p>
+                    <Link to="">Analyze</Link>
+                </div>
+                <div className="right">
+                    <div className="top"/>
+                    <div className="bottom">
+                        <p>Be impowered to make informed decisions:</p>
+                        <ul>
+                            <li>Targeted marketing</li>
+                            <li>Customer care</li>
+                            <li>Personal connections</li>
+                        </ul>
+                        <a href="https://www.ibm.com/watson/developercloud/doc/personality-insights/usecases.html" target="_blank">Learn more</a>
+                    </div>
                 </div>
             </section>
+            <footer>
+                <input type="image" src="https://raw.githubusercontent.com/anhhtle/founding-speeches2/master/public/img/github.png" />
+            </footer>
         </div>
     )
 }
