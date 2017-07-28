3bfd0a6b4235459b9d3ec03b1e9baed4ca09f614
anhhtle <anh.ht.le@gmail.com>
2017-07-28 02:22:02 -0700

    new

--- src/components/main-container/index.js
+++ src/components/main-container/index.js
@@ -4,7 +4,6 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as actions from '../../actions/';
 import {scrollToTop} from '../../utils';
-import Loader from 'react-loader';
 
 import './main-container.css';
 import './main-container-responsive.css';
@@ -40,9 +39,7 @@ export class MainContainer extends React.Component{
         return (
             <section className="main-container">
                 <div className="main-grid">
-                    <Loader loaded={props.loaded} >
-                        {cards}
-                    </Loader >
+                    {cards}
                 </div>
             </section>
         );
@@ -53,8 +50,7 @@ const mapStateToProps = (state, props) => {
     return {
         history: props.history,
         presidents: state.presidents.presidents,
-        error: state.presidents.presidents_error,
-        loaded: state.presidents.loaded
+        error: state.presidents.presidents_error
     }
 };
 
